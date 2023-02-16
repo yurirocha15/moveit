0a99752e860447eecf231351a45f17dc6652569c
Yuri Rocha <yurirocha15@gmail.com>
2023-02-16 13:51:37 +0900

    build working

--- moveit_planners/ompl/ompl_interface/include/moveit/ompl_interface/parameterization/work_space/pose_model_state_space.h
+++ moveit_planners/ompl/ompl_interface/include/moveit/ompl_interface/parameterization/work_space/pose_model_state_space.h
@@ -110,6 +110,10 @@ public:
   void copyToOMPLState(ompl::base::State* state, const moveit::core::RobotState& rstate) const override;
   void sanityChecks() const override;
 
+  unsigned int getNumPositions() const;
+  bool copyPositionsToReals(std::vector<double>& reals, const ompl::base::State* source) const;
+  bool copyPositionsFromReals(ompl::base::State* destination, const std::vector<double>& reals) const;
+
   const std::string& getParameterizationType() const override
   {
     return PARAMETERIZATION_TYPE;
